9922f0c6be14fac0c264c66f7963d78b0f51abce
Pulipuli Chen <pulipuli.chen@gmail.com>
2023-11-11 20:48:15 +0800

    西元2023年11月11日 (週六) 20時48分15秒 CST

--- app/index.js
+++ app/index.js
@@ -90,7 +90,7 @@ let main = async function () {
     if (isCompress === false) {
       let targetFolder = path.resolve(dirname, filenameNoExt)
       while (true) {
-        console.log(targetFolder, fs.existsSync(targetFolder), fs.lstatSync(targetFolder).isDirectory())
+        console.log(targetFolder, fs.existsSync(targetFolder))
         if (targetFolder && fs.existsSync(targetFolder) && fs.lstatSync(targetFolder).isDirectory()) {
           let list = fs.readdirSync(targetFolder)
           if (list.length > 1) {
